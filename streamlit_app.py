8fc1c206177b1ff48e776c7499e56fea9157e6fa
Iustin Nicolaev <99035229+inicolaev@users.noreply.github.com>
2024-09-03 17:38:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+
+names = ["admin", "guest"]
+usernames = ["admin", "guest"]
+passwords = ["123", "456"]
+
+hashed_passwords = stauth.hasher(passwords).generate()
+authenticator = stauth.authenticate(names, usernames, hashed_passwords, "some_cookie_name", "some_signature_key", cookie_expiry_days=30)
+
+name, authentication_status = authenticator.login("Login", "main")
+
+if authentication_status:
+    st.write(f'Welcome *{name}*')
+    st.write("Add your Streamlit dashboard code here")
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
